adbb83c4773e87e8f46fe8e8b7884a90aeded893
rhildred <rhildred@gmail.com>
2018-10-09 13:53:59 -0400

    Made it more of haunted house game

--- Game.js
+++ Game.js
@@ -1,7 +1,8 @@
 const GameState = Object.freeze({
     WELCOMING:   Symbol("welcoming"),
-    STICK:  Symbol("stick"),
-    PLAY: Symbol("play")
+    FLAT:  Symbol("flat"),
+    WAIT: Symbol("wait"),
+    MANSION: Symbol("mansion")
 });
 
 export default class Game{
@@ -11,26 +12,19 @@ export default class Game{
     
     makeAMove(sInput)
     {
-        let sReply = "Hi I'm spot (a dog). Oh look a stick. Do you play or keep on walking?";
+        let sReply = "It is a dark and rainy night. Bang! You have a flat tire. Too bad you don't have a spare. Do you wait or go to the spooky mansion for help?";
         switch(this.stateCur){
             case GameState.WELCOMING:
-                this.stateCur = GameState.STICK;
+                this.stateCur = GameState.FLAT;
                 break;
-            case GameState.STICK:
-                if(sInput.toLowerCase().match("play")){
-                    sReply = "Great my favourite game ... Here's the stick back. Do you throw it again?"
-                    this.stateCur = GameState.PLAY;
+            case GameState.FLAT:
+                if(sInput.toLowerCase().match("wait")){
+                    sReply = "The road is deserted. After 1 hour there is still no help. Do you keep Waiting or do you go to the house?";
                 }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
+                    sReply ="On the door is a large knocker. Do you knock or run back to your car to wait?";
+                    this.stateCur = GameState.MANSION;
                 }
-                break;
-            case GameState.PLAY:
-                if(sInput.toLowerCase().match("yes")){
-                    sReply = "Here it is! I got it for you. Do you toss it again.... Please? ";
-                }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
-                    this.stateCur = GameState.STICK;
-                }
+                this.stateCur = GameState.FLAT;
                 break;
         }
         return(sReply);
